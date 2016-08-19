f3859c37fcd17cb6e9c448728b44c9bec9c4d0be
Thomas Mueller <mueller@adobe.com>
2016-08-19 17:24:43 +0200

    sipHash bugfix

--- src/main/java/org/minperf/universal/StringHash.java
+++ src/main/java/org/minperf/universal/StringHash.java
@@ -55,7 +55,7 @@ public class StringHash implements UniversalHash<String> {
                     m |= ((long) b[off + i] & 255) << (8 * i);
                 }
                 if (i < 8) {
-                    m |= ((long) b.length) << 56;
+                    m |= ((long) end - start) << 56;
                 }
                 v3 ^= m;
                 repeat = 2;
