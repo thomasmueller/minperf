c3889d9279959aa6cdb58fbd35f114dd7cb4a773
Thomas Mueller <mueller@adobe.com>
2016-08-19 17:43:49 +0200

    Test change

--- src/test/java/org/minperf/FunctionInfo.java
+++ src/test/java/org/minperf/FunctionInfo.java
@@ -4,24 +4,25 @@ package org.minperf;
  * The information (time and space usage) of a minimum perfect hash function.
  */
 public class FunctionInfo {
-    
+
     int size;
     int split;
     int firstPartSize;
     int leafSize;
     int loadFactor;
     double bitsPerKey;
+    double headerBitsPerKey;
     double generateNanos;
     double evaluateNanos;
-    
+
     @Override
     public String toString() {
         return "size: " + size +
                 " leafSize: " + leafSize +
                 " loadFactor: " + loadFactor +
                 " bitsPerKey: " + bitsPerKey +
-                " generateSeconds: " + (generateNanos * size / 1_000_000_000) + 
-                " generateNanosPerKey: " + generateNanos + 
+                " generateSeconds: " + (generateNanos * size / 1_000_000_000) +
+                " generateNanosPerKey: " + generateNanos +
                 " evaluateNanosPerKey: " + evaluateNanos;
     }
 
