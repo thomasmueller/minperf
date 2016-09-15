a9323c6c5626d09d6ef2d9ad9943c8cab194a9f7
Thomas Mueller <mueller@adobe.com>
2016-09-15 18:08:29 +0200

    Add a EliasFanoMonotoneList

---
+++ src/test/java/org/minperf/eliasFano/EliasFanoMonotoneList.java
@@ -0,0 +1,69 @@
+package org.minperf.eliasFano;
+
+import java.util.BitSet;
+
+import org.minperf.BitBuffer;
+import org.minperf.rank.SimpleRankSelect;
+
+/**
+ * A list of monotone increasing values. Each entry needs 2 + log(gap) bits,
+ * where gap is the average gap between entries, plus the overhead for the
+ * "select" structure. A lookup needs one "select" lookup, and is otherwise
+ * constant.
+ */
+public class EliasFanoMonotoneList {
+
+    private final BitBuffer buffer;
+    private final int start;
+    private final int lowBitCount;
+    private final SimpleRankSelect select;
+
+    private EliasFanoMonotoneList(BitBuffer buffer, int start, int lowBitCount, SimpleRankSelect select) {
+        this.buffer = buffer;
+        this.start = start;
+        this.lowBitCount = lowBitCount;
+        this.select = select;
+    }
+
+    public static EliasFanoMonotoneList generate(int[] data, BitBuffer buffer) {
+        int len = data.length;
+        // verify it is monotone
+        for (int i = 1; i < len; i++) {
+            if (data[i - 1] > data[i]) {
+                throw new AssertionError();
+            }
+        }
+        buffer.writeEliasDelta(len + 1);
+        int max = data[len - 1];
+        int lowBitCount = 32 - Integer.numberOfLeadingZeros(Integer.highestOneBit(max / len));
+        buffer.writeEliasDelta(lowBitCount + 1);
+        int start = buffer.position();
+        BitSet set = new BitSet();
+        for (int i = 0; i < len; i++) {
+            int x = i + (data[i] >>> lowBitCount);
+            set.set(x);
+        }
+        int mask = (1 << lowBitCount) - 1;
+        for (int i = 0; i < len; i++) {
+            buffer.writeNumber(data[i] & mask, lowBitCount);
+        }
+        SimpleRankSelect select = SimpleRankSelect.generate(set, buffer);
+        return new EliasFanoMonotoneList(buffer, start, lowBitCount, select);
+    }
+
+    public static EliasFanoMonotoneList load(BitBuffer buffer) {
+        int len = (int) (buffer.readEliasDelta() - 1);
+        int lowBitCount = (int) (buffer.readEliasDelta() - 1);
+        int start = buffer.position();
+        buffer.seek(start + len * lowBitCount);
+        SimpleRankSelect select = SimpleRankSelect.load(buffer);
+        return new EliasFanoMonotoneList(buffer, start, lowBitCount, select);
+    }
+
+    public int get(int i) {
+        int low = (int) buffer.readNumber(start + i * lowBitCount, lowBitCount);
+        int high = (int) select.select(i) - i;
+        return (high << lowBitCount) + low;
+    }
+
+}
---
+++ src/test/java/org/minperf/eliasFano/EliasFanoMonotoneListTest.java
@@ -0,0 +1,123 @@
+package org.minperf.eliasFano;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+import org.minperf.BitBuffer;
+
+/**
+ * Test the EliasFanoMonotoneList.
+ */
+public class EliasFanoMonotoneListTest {
+
+    public static void main(String... args) {
+        new EliasFanoMonotoneListTest().test();
+    }
+
+    @Test
+    public void test() {
+        for (int bucketSize = 8; bucketSize < 256; bucketSize *= 2) {
+            for (int size = 100; size <= 100000000; size *= 10) {
+                if (size >= bucketSize) {
+                    test(bucketSize, size);
+                }
+            }
+        }
+    }
+
+    private static void test(int bucketSize, int size) {
+        int bucketCount = size / bucketSize;
+        int[] sizes = randomSizes(size, bucketCount);
+        int[] posList = posList(sizes);
+        int[] expected = expectedPosList(size, bucketCount);
+        int[] offsets = offsets(posList, expected);
+        int min = min(offsets), max = max(offsets);
+        int entryBits = 32 - Integer.numberOfLeadingZeros(-min + max);
+        int diff = min(sizes);
+        int[] sizeOffsets = plus(sizes, -diff);
+        int[] posList2 = posList(sizeOffsets);
+        // System.out.println(size + " avg gap: " + (double) posList2[posList2.length - 1] / posList2.length);
+        for (int i = 0; i < bucketCount; i++) {
+            assertEquals(posList[i], posList2[i] + i * diff);
+        }
+        BitBuffer buffer = new BitBuffer(10 * size);
+        EliasFanoMonotoneList list = EliasFanoMonotoneList.generate(
+                posList2, buffer);
+        int bitCount = buffer.position();
+        double oldBits = (double) (entryBits * bucketCount) / size;
+        double newBits =  (double) bitCount / size;
+        System.out.println("bucketSize " + bucketSize + " bucketCount " + bucketCount
+                + " old " + oldBits + " new " + newBits
+                + " saving " + (oldBits - newBits));
+        for (int i = 0; i < bucketCount; i++) {
+            assertEquals(posList2[i], list.get(i));
+        }
+        buffer.seek(0);
+        list = EliasFanoMonotoneList.load(buffer);
+        for (int i = 0; i < bucketCount; i++) {
+            assertEquals(posList2[i], list.get(i));
+        }
+    }
+
+    private static int[] randomSizes(int size, int bucketCount) {
+        Random r = new Random(size + bucketCount);
+        int[] sizes = new int[bucketCount];
+        for (int i = 0; i < size; i++) {
+            sizes[r.nextInt(bucketCount)]++;
+        }
+        return sizes;
+    }
+
+    private static int[] expectedPosList(int size, int bucketCount) {
+        int[] sizes = new int[bucketCount];
+        for (int i = 0; i < bucketCount; i++) {
+            sizes[i] = (int) ((long) size * i / bucketCount);
+        }
+        return sizes;
+    }
+
+    private static int[] offsets(int[] got, int[] expected) {
+        int[] offsets = new int[got.length];
+        for (int i = 0; i < offsets.length; i++) {
+            offsets[i] = got[i] - expected[i];
+        }
+        return offsets;
+    }
+
+    private static int min(int[] sizes) {
+        int min = Integer.MAX_VALUE;
+        for (int x : sizes) {
+            min = Math.min(min, x);
+        }
+        return min;
+    }
+
+    private static int max(int[] sizes) {
+        int max = Integer.MIN_VALUE;
+        for (int x : sizes) {
+            max = Math.max(max, x);
+        }
+        return max;
+    }
+
+    private static int[] plus(int[] list, int plus) {
+        int[] list2 = new int[list.length];
+        for (int i = 0; i < list.length; i++) {
+            list2[i] = list[i] + plus;
+        }
+        return list2;
+    }
+
+    private static int[] posList(int[] sizes) {
+        int[] posList = new int[sizes.length];
+        int p = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            posList[i] = p;
+            p += sizes[i];
+        }
+        return posList;
+    }
+
+}
