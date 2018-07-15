34e7bf4c274a1f9e49e0a863ff583e3dc7d2204e
Thomas Mueller <mueller@adobe.com>
2018-07-15 20:05:42 +0200

    Allow tests in different packages

--- src/main/java/org/minperf/bloom/Filter.java
+++ src/main/java/org/minperf/bloom/Filter.java
@@ -5,46 +5,46 @@ public interface Filter {
     boolean mayContain(long key);
     long getBitCount();
 
-    enum Type {
+    public enum Type {
+        XOR8 {
+            @Override
+            public Filter construct(long[] keys, int setting) {
+                return XorFilter_8bit.construct(keys);
+            }
+        },
+        BLOOM {
+            @Override
+            public Filter construct(long[] keys, int setting) {
+                return BloomFilter.construct(keys, setting);
+            }
+        },
         CUCKOO16_4 {
             @Override
-            Filter construct(long[] keys, int setting) {
+            public Filter construct(long[] keys, int setting) {
                 return CuckooFilter_16bit_4entries.construct(keys);
             }
         },
         CUCKOO8_4 {
             @Override
-            Filter construct(long[] keys, int setting) {
+            public Filter construct(long[] keys, int setting) {
                 return CuckooFilter_8bit_4entries.construct(keys);
-            }
-        },
-        XOR8 {
-            @Override
-            Filter construct(long[] keys, int setting) {
-                return XorFilter_8bit.construct(keys);
             }
         },
         XOR16 {
             @Override
-            Filter construct(long[] keys, int setting) {
+            public Filter construct(long[] keys, int setting) {
                 return XorFilter_16bit.construct(keys);
-            }
-        },
-        BLOOM {
-            @Override
-            Filter construct(long[] keys, int setting) {
-                return BloomFilter.construct(keys, setting);
             }
         },
         XOR {
             @Override
-            Filter construct(long[] keys, int bitsPerKey) {
+            public Filter construct(long[] keys, int bitsPerKey) {
                 return XorFilter.construct(keys, bitsPerKey);
             }
         },
         CUCKOO {
             @Override
-            Filter construct(long[] keys, int setting) {
+            public Filter construct(long[] keys, int setting) {
                 return CuckooFilter.construct(keys, setting);
             }
         },
@@ -63,7 +63,7 @@ public interface Filter {
          * @param setting the setting (roughly bits per fingerprint)
          * @return the constructed filter
          */
-        abstract Filter construct(long[] keys, int setting);
+        public abstract Filter construct(long[] keys, int setting);
 
     }
 
